590bc628b9ec0bcd9f6120d4a8fda14eaee2c1b0
thanhdathd <imthanhhai217@gmail.com>
2021-05-20 19:19:47 +0700

    add logic on list nguyen lieu 80%

--- src/views/shop/nguyenlieu/NguyenLieu.js
+++ src/views/shop/nguyenlieu/NguyenLieu.js
@@ -7,12 +7,13 @@ import {
   CCardHeader,
   CCardBody,
   CDataTable,
-  CBadge
+  CBadge, CPagination, CNav, CNavItem, CNavLink, CButton, CCollapse
 } from '@coreui/react'
 import { rgbToHex } from '@coreui/utils'
 import axios from 'axios'
 
 const HOST = 'http://localhost:5000'
+const perPage = 10; // item per page
 
 
 const ThemeView = () => {
@@ -52,25 +53,93 @@ const ThemeColor = ({className, children}) => {
   )
 }
 
-const fields = ['id', 'name', 'quantity','unit'];
+const fields = [{key: 'id', _style: {width: '5%'}}, 'name',
+ {key: 'quantity', _style: { width: '10%'}},
+ {key: 'unit', _style: {width: '10%'}}, 
+{key: 'status', _style:{width: '5%'}},
+{
+  key: 'row_action',
+  label: 'Action',
+  _style: { width: '1%' },
+  sorter: false,
+  filter: false
+}];
 
 const NguyenLieu = () => {
   const [materialList, setMaterials] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPage, setTotalPage] = useState(1);
+  const [catList, setCatList] = useState([]);
+  const [activeCat, setActiveCat] = useState(-1)
+  const [catId, setCatId] = useState(-1)
+  const [editIdx, setEditIdx] = useState([])
 
   useEffect(() => {
     console.log(' Bat dau load nguyen lieu')
-    axios.get(`${HOST}/api/material/list?page=1&size=10`)
+    loadDataPage(currentPage, catId)
+  }, [currentPage, catId])
+
+  const loadDataPage = (page, catId) => {
+    axios.get(`${HOST}/api/material/list?page=${page}&size=${perPage}&cat=${catId}`)
     .then(res => {
       if(res.status == 200){
         console.log('tai data nguyen lieu thanh cong')
         console.log(JSON.stringify(res.data))
         setMaterials(res.data.items)
+        setTotalPage(res.data.totalPages)
       }else{
         console.log('load nguyen lieu ko thanh cong')
       }
     })
     .catch(err => console.log('fail to load material'))
+  }
+
+  useEffect(() => {
+    loadCat()
   }, [])
+
+  const loadCat = () => {
+    axios.get(`${HOST}/api/materialcat/list`)
+    .then(res => {
+      if(res.status == 200){
+        console.log(JSON.stringify(res.data.materialCat))
+        setCatList(res.data.materialCat)
+      }else{
+        console.log('Load material category list err')
+      }
+    })
+    .catch(err => console.log('Load material category list err: '+err))
+  }
+
+  const navClick = (idx, catId) => {
+    setActiveCat(idx)
+    setCatId(catId)
+  }
+
+  const toggleEdit = (index) => {
+    const position = editIdx.indexOf(index)
+    let newEditIdx = editIdx.slice()
+    if (position !== -1) {
+      newEditIdx.splice(position, 1)
+    } else {
+      newEditIdx = [...editIdx, index]
+    }
+    setEditIdx(newEditIdx)
+  }
+
+  const getBadge = (status)=>{
+    if(status > 5) return 'success'
+    else if (status > 2) return 'warning'
+    else return 'danger'
+
+    // switch (status) {
+    //   case 'Active': return 'success'
+    //   case 'Inactive': return 'secondary'
+    //   case 'Pending': return 'warning'
+    //   case 'Banned': return 'danger'
+    //   default: return 'primary'
+    // }
+  }
 
   return (
     <>
@@ -78,25 +147,83 @@ const NguyenLieu = () => {
         <CCol>
           <CCard>
             <CCardHeader>
-              Nguyen Lieu
+              <CRow >
+                <CCol sm="12" lg="4">
+                  <h2>Nguyên Liệu</h2>
+                </CCol>
+                <CCol sm="12" lg="8" >
+                  <CNav variant="pills" className='border border-bg-gray-300 justify-content-end' style={{width: '100%', justifyContent: 'right'}}>
+                      <CNavItem>
+                        <CNavLink onClick={() => navClick(-1, -1)} active={activeCat === -1}>Tất cả</CNavLink>
+                      </CNavItem>
+                    {catList.map((cat, idx )=> {
+                       return (
+                       <CNavItem style={{marginLeft: 10}}>
+                        <CNavLink active={idx === activeCat} onClick={() => navClick(idx, cat.id)}>{cat.name}</CNavLink>
+                      </CNavItem>
+                      )
+                    })}
+                  </CNav>
+                </CCol>
+              </CRow>
             </CCardHeader>
             <CCardBody>
             <CDataTable
               items={materialList}
               fields={fields}
-              itemsPerPage={10}
-              pagination
+              itemsPerPage={perPage}
+              sorter
               scopedSlots = {{
                 'status':
                   (item)=>(
                     <td>
-                      <CBadge color={'success'}>
-                        {item.unit}
+                      <CBadge color={getBadge(item.quantity)}>
+                        {item.quantity > 5 ? 'good': 'bad'}
                       </CBadge>
                     </td>
-                  )
+                  ),
+                  'row_action':
+                    (item, index)=>{
+                      return (
+                        <td className="py-2">
+                          <CButton
+                            color="primary"
+                            variant="outline"
+                            shape="square"
+                            size="sm"
+                            onClick={()=>{toggleEdit(index)}}
+                          >
+                            {editIdx.includes(index) ? 'Lưu' : 'Sửa'}
+                          </CButton>
+                        </td>
+                        )
+                    },
+                    'details':
+                      (item, index)=>{
+                        return (
+                        <CCollapse show={editIdx.includes(index)}>
+                          <CCardBody>
+                            <h4>
+                              {item.name}
+                            </h4>
+                            <p className="text-muted">User since: {item.updatedAt}</p>
+                            <CButton size="sm" color="info">
+                              User Settings
+                            </CButton>
+                            <CButton size="sm" color="danger" className="ml-1">
+                              Delete
+                            </CButton>
+                          </CCardBody>
+                        </CCollapse>
+                      )
+                    }  
 
               }}
+            />
+            <CPagination
+                activePage={currentPage}
+                pages={totalPage}
+                onActivePageChange={(i) => setCurrentPage(i)}
             />
             </CCardBody>
           </CCard>
