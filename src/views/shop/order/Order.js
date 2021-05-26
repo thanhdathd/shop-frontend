616088380d8b77a550fa6fe0260c416d4afd2695
thanhdathd <imthanhhai217@gmail.com>
2021-05-26 19:00:50 +0700

    fix time, and price on orrder page

--- src/views/shop/order/Order.js
+++ src/views/shop/order/Order.js
@@ -10,48 +10,13 @@ import {
 } from '@coreui/react'
 import { rgbToHex } from '@coreui/utils'
 import axios from 'axios'
+import moment from 'moment';
+
 //import { DocsLink } from 'src/reusable'
 
 const HOST = 'http://localhost:5000'
 const PER_PAGE = 10;
 
-
-const ThemeView = () => {
-  const [color, setColor] = useState('rgb(255, 255, 255)')
-  const ref = createRef()
-
-  useEffect(() => {
-    const el = ref.current.parentNode.firstChild
-    const varColor = window.getComputedStyle(el).getPropertyValue('background-color')
-    setColor(varColor)
-  }, [ref])
-
-  return (
-    <table className="table w-100" ref={ref}>
-      <tbody>
-      <tr>
-        <td className="text-muted">HEX:</td>
-        <td className="font-weight-bold">{ rgbToHex(color) }</td>
-      </tr>
-      <tr>
-        <td className="text-muted">RGB:</td>
-        <td className="font-weight-bold">{ color }</td>
-      </tr>
-      </tbody>
-    </table>
-  )
-}
-
-const ThemeColor = ({className, children}) => {
-  const classes = classNames(className, 'theme-color w-75 rounded mb-3')
-  return (
-    <CCol xl="2" md="4" sm="6" xs="12" className="mb-4">
-      <div className={classes} style={{paddingTop: '75%'}}></div>
-      {children}
-      <ThemeView/>
-    </CCol>
-  )
-}
 
 const fields = [
     {key: 'id', _style: {width: '5%'}},
@@ -59,7 +24,7 @@ const fields = [
     {key: 'name', _style: {width: '30%'}},
     {key: 'option', _style: {width: '10%'}},
     {key: 'quantity', _style: {width: '10%'}},
-    {key: 'price', _style: {width: '10%'}},
+    {key: 'price', label: 'Payment',  _style: {width: '10%'}},
     {key: 'status', _style: {width: '10%'}},
     {key: 'action', _style: {width: '15%'}},
 ]
@@ -86,7 +51,7 @@ const [totalPage, setTotalPage] = useState(1)
 
 useEffect(() => {
     _currentPage !== page && setPage(_currentPage)
-    var time = formater.format('2021-04-28T09:35:23.000Z')
+    var time = moment('2021-05-29T09:35:23.000Z').format('hh:mm DD/MM/yyyy')
     console.log('time:'+time)
   }, [_currentPage, page])
 
@@ -99,12 +64,24 @@ const getPageData = (page, filter, orderBy) => {
     .then(res => {
         if(res.status == 200){
             console.log('tai orrder list thanh cong');
+            preProcess(res.data.items)
             setListOrder(res.data.items)
             setTotalPage(res.data.totalPages)
         }else{
             console.log('tai order loi')
         }
     }).catch(e => console.log('err:'+e.message))
+}
+
+const preProcess = (listData) => {
+  listData.forEach(d => {
+    d.time = moment(d.createdAt).format('hh:mm DD/MM/yyyy');
+    var price = 0
+    d.listProduct.forEach(p => {
+      price += p.price
+    });
+    d.price = price
+  })
 }
 
 const pageChange = newPage => {
@@ -160,7 +137,7 @@ const pageChange = newPage => {
                                 'name': (item) => (
                                     <td>
                                         <div className="font-weight-bold">{item.name}</div>
-                                        <div className="small text-muted">Staff: {item.staffName} | {item.createdAt}</div>
+                                        <div className="small text-muted">Staff: {item.staffName} | {item.time}</div>
                                     </td>
                                 ),
                                 'price': (item) => (
@@ -209,84 +186,7 @@ const pageChange = newPage => {
                 </CCard>
             </CCol>
         </CRow>
-
-
-
-
-
-
-
-
-      <CCard>
-        <CCardHeader>
-          <h1>Order Khach hang</h1>
-          {/* <DocsLink href="https://coreui.io/docs/utilities/colors/"/> */}
-        </CCardHeader>
-        <CCardBody>
-          <CRow>
-            <ThemeColor className="bg-primary">
-              <h6>Brand Primary Color</h6>
-            </ThemeColor>
-            <ThemeColor className="bg-secondary">
-              <h6>Brand Secondary Color</h6>
-            </ThemeColor>
-            <ThemeColor className="bg-success">
-              <h6>Brand Success Color</h6>
-            </ThemeColor>
-            <ThemeColor className="bg-danger">
-              <h6>Brand Danger Color</h6>
-            </ThemeColor>
-            <ThemeColor className="bg-warning">
-              <h6>Brand Warning Color</h6>
-            </ThemeColor>
-            <ThemeColor className="bg-info">
-              <h6>Brand Info Color</h6>
-            </ThemeColor>
-            <ThemeColor className="bg-light">
-              <h6>Brand Light Color</h6>
-            </ThemeColor>
-            <ThemeColor className="bg-dark">
-              <h6>Brand Dark Color</h6>
-            </ThemeColor>
-          </CRow>
-        </CCardBody>
-      </CCard>
-      <CCard>
-        <CCardHeader>
-          Grays
-        </CCardHeader>
-        <CCardBody>
-          <CRow className="mb-3">
-            <ThemeColor className="bg-gray-100">
-              <h6>Gray 100 Color</h6>
-            </ThemeColor>
-            <ThemeColor className="bg-gray-200">
-              <h6>Gray 200 Color</h6>
-            </ThemeColor>
-            <ThemeColor className="bg-gray-300">
-              <h6>Gray 300 Color</h6>
-            </ThemeColor>
-            <ThemeColor className="bg-gray-400">
-              <h6>Gray 400 Color</h6>
-            </ThemeColor>
-            <ThemeColor className="bg-gray-500">
-              <h6>Gray 500 Color</h6>
-            </ThemeColor>
-            <ThemeColor className="bg-gray-600">
-              <h6>Gray 600 Color</h6>
-            </ThemeColor>
-            <ThemeColor className="bg-gray-700">
-              <h6>Gray 700 Color</h6>
-            </ThemeColor>
-            <ThemeColor className="bg-gray-800">
-              <h6>Gray 800 Color</h6>
-            </ThemeColor>
-            <ThemeColor className="bg-gray-900">
-              <h6>Gray 900 Color</h6>
-            </ThemeColor>
-          </CRow>
-        </CCardBody>
-      </CCard>
+ 
     </>
   )
 }
